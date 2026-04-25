b0d121a552c082e897851e4aad851748b3d43ad3
greyray96-source <grey.ray96@gmail.com>
2026-04-25 18:39:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 mkdir -p /root/.garminconnect
 echo "$GARMINTOKENS_BASE64" | base64 -d > /root/.garminconnect/garmin_tokens.json
-export GARMIN_MCP_PORT=$PORT
-export HOST=0.0.0.0
-exec garmin-mcp --transport sse
+exec garmin-mcp
