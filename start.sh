3be8a284e25fc28b4f7037eeab36a7aa17bd783f
greyray96-source <grey.ray96@gmail.com>
2026-04-25 18:00:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ~/.garminconnect
-echo "$GARMIN_TOKENS" > ~/.garminconnect/garmin_tokens.json
+echo "$GARMINTOKENS_BASE64" | base64 -d > ~/.garminconnect/garmin_tokens.json
 garmin-mcp --transport streamable-http --host 0.0.0.0 --port $PORT
